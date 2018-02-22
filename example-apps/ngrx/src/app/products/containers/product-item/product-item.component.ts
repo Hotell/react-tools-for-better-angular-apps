4400150f737154fb5c905fbf26caffd4b8c06794
Martin Hochel <hochelmartin@gmail.com>
2018-02-22 23:37:26 +0100

    feat(example-apps): add working ngrx example app with latest angular

---
+++ example-apps/ngrx/src/app/products/containers/product-item/product-item.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { tap } from 'rxjs/operators';
+import * as fromStore from '../../store';
+
+import { Pizza } from '../../models/pizza.model';
+import { Topping } from '../../models/topping.model';
+
+@Component({
+  selector: 'product-item',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['product-item.component.scss'],
+  template: `
+    <div 
+      class="product-item">
+      <pizza-form
+        [pizza]="pizza$ | async"
+        [toppings]="toppings$ | async"
+        (selected)="onSelect($event)"
+        (create)="onCreate($event)"
+        (update)="onUpdate($event)"
+        (remove)="onRemove($event)">
+        <pizza-display
+          [pizza]="visualise$ | async">
+        </pizza-display>
+      </pizza-form>
+    </div>
+  `,
+})
+export class ProductItemComponent implements OnInit {
+  pizza$: Observable<Pizza>;
+  visualise$: Observable<Pizza>;
+  toppings$: Observable<Topping[]>;
+
+  constructor(private store: Store<fromStore.ProductsState>) {}
+
+  ngOnInit() {
+    this.pizza$ = this.store.select(fromStore.getSelectedPizza).pipe(
+      tap((pizza: Pizza = null) => {
+        const pizzaExists = !!(pizza && pizza.toppings);
+        const toppings = pizzaExists
+          ? pizza.toppings.map(topping => topping.id)
+          : [];
+        this.store.dispatch(new fromStore.VisualiseToppings(toppings));
+      })
+    );
+    this.toppings$ = this.store.select(fromStore.getAllToppings);
+    this.visualise$ = this.store.select(fromStore.getPizzaVisualised);
+  }
+
+  onSelect(event: number[]) {
+    this.store.dispatch(new fromStore.VisualiseToppings(event));
+  }
+
+  onCreate(event: Pizza) {
+    this.store.dispatch(new fromStore.CreatePizza(event));
+  }
+
+  onUpdate(event: Pizza) {
+    this.store.dispatch(new fromStore.UpdatePizza(event));
+  }
+
+  onRemove(event: Pizza) {
+    const remove = window.confirm('Are you sure?');
+    if (remove) {
+      this.store.dispatch(new fromStore.RemovePizza(event));
+    }
+  }
+}
---
+++ example-apps/ngrx/src/app/products/containers/products/products.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import * as fromStore from '../../store';
+import { Pizza } from '../../models/pizza.model';
+
+@Component({
+  selector: 'products',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['products.component.scss'],
+  template: `
+    <div class="products">
+      <div class="products__new">
+        <a
+          class="btn btn__ok" 
+          routerLink="./new">
+          New Pizza
+        </a>
+      </div>
+      <div class="products__list">
+        <div *ngIf="!((pizzas$ | async)?.length)">
+          No pizzas, add one to get started.
+        </div>
+        <pizza-item
+          *ngFor="let pizza of (pizzas$ | async)"
+          [pizza]="pizza">
+        </pizza-item>
+      </div>
+    </div>
+  `,
+})
+export class ProductsComponent implements OnInit {
+  pizzas$: Observable<Pizza[]>;
+
+  constructor(private store: Store<fromStore.ProductsState>) {}
+
+  ngOnInit() {
+    this.pizzas$ = this.store.select(fromStore.getAllPizzas);
+  }
+}
