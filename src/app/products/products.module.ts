d86dad35c03e06c93ac8cbf02a0627d4897453c5
Martin Hochel <hochelmartin@gmail.com>
2018-02-23 17:42:15 +0100

    feat(products): register entities to module

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductsComponent } from './products.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
+// guards
+import * as fromGuards from './guards';
 // services
 import * as fromServices from './services';
+// stateless components
+import * as fromComponents from './components';
 
 export const ROUTES: Routes = [
   {
@@ -13,21 +19,21 @@ export const ROUTES: Routes = [
     // canActivate: [fromGuards.PizzasGuard],
     component: ProductsComponent,
   },
-  // {
-  //   path: 'new',
-  //   canActivate: [fromGuards.PizzasGuard, fromGuards.ToppingsGuard],
-  //   component: fromContainers.ProductItemComponent,
-  // },
-  // {
-  //   path: ':pizzaId',
-  //   canActivate: [fromGuards.PizzaExistsGuards, fromGuards.ToppingsGuard],
-  //   component: fromContainers.ProductItemComponent,
-  // },
+  {
+    path: 'new',
+    //   canActivate: [fromGuards.PizzasGuard, fromGuards.ToppingsGuard],
+    component: ProductItemComponent,
+  },
+  {
+    path: ':pizzaId',
+    //   canActivate: [fromGuards.PizzaExistsGuards, fromGuards.ToppingsGuard],
+    component: ProductItemComponent,
+  },
 ];
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(ROUTES)],
-  providers: [...fromServices.services],
-  declarations: [ProductsComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(ROUTES)],
+  providers: [...fromServices.services, ...fromGuards.guards],
+  declarations: [ProductsComponent, ProductItemComponent, ...fromComponents.components],
 })
 export class ProductsModule {}
