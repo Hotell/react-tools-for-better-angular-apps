dffb4f60de1e5020ce2d7e3a1700bb33acc9b140
Martin Hochel <hochelmartin@gmail.com>
2018-02-23 17:21:40 +0100

    feat(products): add guards

---
+++ src/app/products/guards/pizzas.guard.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
+
+import { NgRedux } from '@angular-redux/store';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { tap, filter, take, switchMap, catchError } from 'rxjs/operators';
+
+import * as fromStore from '../store';
+
+@Injectable()
+export class PizzasGuard implements CanActivate {
+  constructor(private store: NgRedux<fromStore.ProductsState>) {}
+
+  canActivate(): Observable<boolean> {
+    return this.checkStore().pipe(switchMap(() => of(true)), catchError(() => of(false)));
+  }
+
+  checkStore(): Observable<boolean> {
+    return this.store.select(fromStore.getPizzasLoaded).pipe(
+      tap((loaded) => {
+        if (!loaded) {
+          this.store.dispatch(new fromStore.LoadPizzas());
+        }
+      }),
+      filter((loaded) => loaded),
+      take(1)
+    );
+  }
+}
