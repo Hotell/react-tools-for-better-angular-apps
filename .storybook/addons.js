314e8b4d341938be0f488e788e5db2be79921632
Martin Hochel <hochelmartin@gmail.com>
2018-04-12 10:11:08 +0200

    chore(npm): bump to latest storybook, prettier, cli

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,4 +1,3 @@
-// @TODO https://github.com/storybooks/storybook/issues/3047
 import '@storybook/addon-storysource/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -11,42 +11,31 @@ const moduleMocks: NgModule = {
   providers: [PROVIDERS],
 };
 describe('AppComponent', () => {
-  beforeEach(
-    async(() => {
-      TestBed.configureTestingModule({
-        declarations: [AppComponent, moduleMocks.declarations],
-        providers: moduleMocks.providers,
-      }).compileComponents();
-    })
-  );
-  it(
-    'should create the app',
-    async(() => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.debugElement.componentInstance;
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent, moduleMocks.declarations],
+      providers: moduleMocks.providers,
+    }).compileComponents();
+  }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
 
-      expect(app).toBeTruthy();
-      expect(fixture).toMatchSnapshot();
-    })
-  );
-  it(
-    `should have as title 'app'`,
-    async(() => {
-      const fixture = TestBed.createComponent(AppComponent);
-      const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+    expect(fixture).toMatchSnapshot();
+  }));
+  it(`should have as title 'app'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
 
-      expect(app.title).toEqual('app');
-    })
-  );
-  it(
-    'should render title in a h1 tag',
-    async(() => {
-      const fixture = TestBed.createComponent(AppComponent);
-      fixture.detectChanges();
-      const compiled = fixture.debugElement.nativeElement;
+    expect(app.title).toEqual('app');
+  }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
 
-      expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-      expect(fixture).toMatchSnapshot();
-    })
-  );
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(fixture).toMatchSnapshot();
+  }));
 });
