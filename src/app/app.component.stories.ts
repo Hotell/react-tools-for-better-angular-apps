01e38e0655045d93f496c02d1425a71b57a55228
Martin Hochel <hochelmartin@gmail.com>
2018-04-12 10:21:35 +0200

    fix(app): make root component story work again

--- src/app/app.component.stories.ts
+++ src/app/app.component.stories.ts
@@ -1,8 +1,12 @@
 import { storiesOf } from '@storybook/angular';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core';
 
 storiesOf('App Component', module).add('root app component', () => ({
+  moduleMetadata: {
+    imports: [CoreModule.forRoot()],
+  },
   component: AppComponent,
   props: {},
 }));
